08040825334d51ca674deec0ed7bc0271fe8c5ff
donjoe <giumontanaro@gmail.com>
2019-03-08 22:47:33 +0100

    Initialize score

--- js/app.js
+++ js/app.js
@@ -1,8 +1,10 @@
 import {playersButtonsInit} from './playersButtons/playersButtons.js';
 import {gridButtonsInit} from './gameGrid/gameGrid.js';
+import {scoreInit} from './score/score.js';
 
 
 document.addEventListener('DOMContentLoaded', () => {
     playersButtonsInit();
     gridButtonsInit();
+    scoreInit();
 });
