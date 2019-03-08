e7bb39b58ff1d2905c6e8c3f599b58da018caf92
donjoe <giumontanaro@gmail.com>
2019-03-08 22:13:25 +0100

    Define a module for playersButtons with the major functionalities

---
+++ js/playersButtons/playersButtons.js
@@ -0,0 +1,41 @@
+import { hideChoosePlayer } from '../choosePlayer/choosePlayer.js';
+
+const playerX = document.getElementById('X');
+const playerO = document.getElementById('O');
+let user;
+let computer;
+
+function deactivatePlayersButtons() {
+    playerX.disabled = true;
+    playerO.disabled = true;
+}
+
+function setPlayer(e) {
+    if (e.target.id === 'X') {
+        user = 'X';
+        computer = 'O';
+        playerX.classList.add('btn-primary');
+        playerX.classList.remove('btn-secondary');
+    } else if (e.target.id === 'O') {
+        user = 'O';
+        computer = 'X';
+        playerO.classList.add('btn-primary');
+        playerO.classList.remove('btn-secondary');
+    }
+}
+
+export function playersButtonsInit() {
+    //change player to X and computer to O
+    playerX.addEventListener('click', (e) => {
+        setPlayer(e);
+        hideChoosePlayer();
+        deactivatePlayersButtons();
+    });
+
+    //change player to O and computer to X
+    playerO.addEventListener('click', (e) => {
+        setPlayer(e);
+        hideChoosePlayer();
+        deactivatePlayersButtons();
+    });
+}
