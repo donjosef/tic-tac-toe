90a54af7c72143c2b3a4f88c3fbc01e64eddf825
donjoe <giumontanaro@gmail.com>
2019-03-08 22:39:27 +0100

    Define module for winMessage

---
+++ js/winMessage/winMessage.js
@@ -0,0 +1,9 @@
+const win_message = document.getElementById('win-message');
+
+export function setTextContent(turn, player, combination) {
+    if(!combination) { //condition for draw game
+        win_message.textContent = `${turn}`;
+    } else {
+        win_message.textContent = `${turn} ${player} wins with ${combination.join('-')}`;
+    }
+}
