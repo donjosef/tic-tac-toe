c76e8ff1d91334f0b61920752b07a5b300c6876e
donjoe <giumontanaro@gmail.com>
2019-03-09 18:52:37 +0100

    Use localStorage to persist score

--- js/score/score.js
+++ js/score/score.js
@@ -1,21 +1,23 @@
-let userScore = 0;
-let computerScore = 0;
+let userScore = Number(localStorage.getItem('userScore'))  || 0;
+let computerScore = Number(localStorage.getItem('computerScore')) || 0;
 const userScore_span = document.getElementById('score-user');
 const computerScore_span = document.getElementById('score-computer');
 
 export function setScore(player) {
     if(player === 'user') {
         userScore += 1;
+        localStorage.setItem('userScore', userScore);
     } else if(player === 'computer') {
         computerScore += 1;
+        localStorage.setItem('computerScore', computerScore);
     }
 }
 
 export function displayScore(player) {
     if(player === 'user') {
-        userScore_span.textContent = userScore;
+        userScore_span.textContent = localStorage.getItem('userScore') || userScore;
     } else if(player === 'computer') {
-        computerScore_span.textContent = computerScore;
+        computerScore_span.textContent = localStorage.getItem('computerScore') || computerScore;
     }
 }
 
