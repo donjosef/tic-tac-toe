f1e182a19e023626e8237734728323b9d8151454
donjoe <giumontanaro@gmail.com>
2019-03-09 18:22:05 +0100

    Initialize reset module

--- js/app.js
+++ js/app.js
@@ -1,10 +1,12 @@
 import {playersButtonsInit} from './playersButtons/playersButtons.js';
 import {gridButtonsInit} from './gameGrid/gameGrid.js';
 import {scoreInit} from './score/score.js';
+import {resetInit} from './reset/reset.js'
 
 
 document.addEventListener('DOMContentLoaded', () => {
     playersButtonsInit();
     gridButtonsInit();
     scoreInit();
+    resetInit();
 });
